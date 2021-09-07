fa26af056599e96012f901999cc226c14bb18e62
Pedro Vaz <holo_vaz@outlook.com>
2021-09-07 11:04:00 -0300

    Adiciona estrutura de interação com o usuário

--- interpretadorCSV.sh
+++ interpretadorCSV.sh
@@ -10,3 +10,38 @@ else
         exit 1
     fi
 fi
+
+# Menu de interacao
+declare -i menu=1
+while [[ menu -ne 0 ]]; do
+    echo -ne "**** Menu ****"
+    echo -ne "\n1 - Buscar por nome;"
+    echo -ne "\n2 - Buscar por email;"
+    echo -ne "\n3 - Agrupar por DDD;"
+    echo -ne "\n4 - Agrupar por mês de aniversario;"
+    echo -ne "\n0 - Sair.\nOpção de entrada: "
+
+    read menu;
+
+    case $menu in
+	1)
+		echo -ne "\nRealizando busca por nome:\n\n"
+		;;
+	2)
+		echo -ne "\nRealizando busca por email:\n\n"
+		;;
+    3)
+		echo -ne "\nRealizando agrupamento por DDD:\n\n"
+		;;
+    4)
+		echo -ne "\nRealizando agrupamento mensal de aniversários:\n\n"
+		;;
+    0)
+		echo -ne "\nFim do programa.\n"
+		;;
+	*)
+		echo -ne "\nEntrada inválida. Tente novamente.\n\n"
+		;;
+  esac
+
+done
