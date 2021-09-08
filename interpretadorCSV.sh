14c0f740820c37477b47330085ed789e37889ef8
Pedro Vaz <holo_vaz@outlook.com>
2021-09-07 22:23:52 -0300

    Corrige erro de leitura do grep

--- interpretadorCSV.sh
+++ interpretadorCSV.sh
@@ -1,46 +1,46 @@
 # Verifica parametro com .csv da agenda
 if [ $# -eq 0 ]; then
-    echo -ne "\nErro. Nenhum arquivo vou passado por parametro.\n"
-    exit 1
+	echo -ne "\nErro. Nenhum arquivo vou passado por parametro.\n"
+	exit 1
 else
-    if [[ -f "$1" ]]; then
-        echo -ne "Lendo agenda $1\n\n"
-    else
-        echo -ne "Erro. O arquivo passado por parametro nao existe.\n"
-        exit 1
-    fi
+	if [[ -f "$1" ]]; then
+		echo -ne "Lendo agenda $1\n\n"
+	else
+		echo -ne "Erro. O arquivo passado por parametro nao existe.\n"
+		exit 1
+	fi
 fi
 
 # Menu de interacao
 declare -i menu=1
 while [[ menu -ne 0 ]]; do
-    echo -ne "**** Menu ****"
-    echo -ne "\n1 - Buscar por nome;"
-    echo -ne "\n2 - Buscar por email;"
-    echo -ne "\n3 - Agrupar por DDD;"
-    echo -ne "\n4 - Agrupar por mês de aniversario;"
-    echo -ne "\n0 - Sair.\nOpção de entrada: "
+	echo -ne "**** Menu ****"
+	echo -ne "\n1 - Buscar por nome;"
+	echo -ne "\n2 - Buscar por email;"
+	echo -ne "\n3 - Agrupar por DDD;"
+	echo -ne "\n4 - Agrupar por mês de aniversario;"
+	echo -ne "\n0 - Sair.\nOpção de entrada: "
 
-    read menu;
+	read menu;
 
-    case $menu in
+	case $menu in
 	1)
 		echo -ne "\nRealizando busca por nome: "
-    read nome
-    grep -i ${nome} $1 | awk -F "," '{print "Nome: " $1 "\nTelefone: " $2 "\nEmail: " $3 "\nAniversário: " $4 "\n\n"}' 
+	read nome
+	grep -i "${nome}" $1 | awk -F "," '{print "Nome: " $1 "\nTelefone: " $2 "\nEmail: " $3 "\nAniversário: " $4 "\n\n"}' 
 		;;
 	2)
 		echo -ne "\nRealizando busca por email: "
-    read email
-    grep -i ${email} $1 | awk -F "," '{print "Nome: " $1 "\nTelefone: " $2 "\nEmail: " $3 "\nAniversário: " $4 "\n\n"}' 
+	read email
+	grep -i "${email}" $1 | awk -F "," '{print "Nome: " $1 "\nTelefone: " $2 "\nEmail: " $3 "\nAniversário: " $4 "\n\n"}' 
 		;;
-    3)
+	3)
 		echo -ne "\nRealizando agrupamento por DDD:\n\n"
 		;;
-    4)
+	4)
 		echo -ne "\nRealizando agrupamento mensal de aniversários:\n\n"
 		;;
-    0)
+	0)
 		echo -ne "\nFim do programa.\n"
 		;;
 	*)
